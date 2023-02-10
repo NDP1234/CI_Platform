312a9062bdd4b35d432df1afd7f43660ed857763
NDP1234 <niravdpatel632@gmail.com>
2023-02-10 17:43:56 +0530

    HTML login page in MVC UI

---
+++ MVC/CI PLATFORM/CI_PLATFORM/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_PLATFORM.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+    }
+}
